c186e2553fd62ca366ce740bb5d38c11dbbc3416
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-18 17:23:38 +0400

    task5.03

---
+++ Lr5/task5.03.sql
@@ -0,0 +1,8 @@
+/*Рассчитайте для каждого члена количество рекомендаций, данных этим 
+членом клуба, искючив членов, которые не давали рекомендаций.*/
+USE cd;
+SELECT CONCAT(mem.firstname, ' ', mem.surname) AS 'ФИО', COUNT(mem.memid) AS 'Кол-во рекомендаций'
+FROM members AS mem
+LEFT JOIN members AS mem1 ON mem1.recommendedby = mem.memid
+WHERE mem1.memid IS NOT NULL
+GROUP BY mem.memid;
