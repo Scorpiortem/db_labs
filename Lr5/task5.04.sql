cd34b2bd0c5deb984c93eec83fbfee48a73dfb11
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-20 12:29:50 +0400

    task5.4

--- Lr5/task5.04.sql
+++ Lr5/task5.04.sql
@@ -2,6 +2,5 @@
 USE cd;
 SELECT facility AS 'Объекты клуба', SUM(book.slots) AS 'Кол-во аренд'
 FROM facilities AS fac
-LEFT JOIN bookings AS book ON book.facid = fac.facid
-WHERE book.slots IS NOT NULL
+INNER JOIN bookings AS book ON book.facid = fac.facid
 GROUP BY fac.facid;
