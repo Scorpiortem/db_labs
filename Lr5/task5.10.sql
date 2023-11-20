54dc5cdfc8af4b6803110b8876d7af2eb2eacb65
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-20 19:38:14 +0400

    task5.10

--- Lr5/task5.10.sql
+++ Lr5/task5.10.sql
@@ -5,24 +5,11 @@
 месяца и слотов, отсортированных по идентификатору и месяцу. При вычислении 
 агрегированных значений для всех месяцев и всех facid возвращайте нулевые 
 значения в столбцах месяца и facid.*/
-USE cd;
-SELECT facid, 
-       CASE WHEN starttime LIKE '2012-01%' THEN '2012-01'
-            WHEN starttime LIKE '2012-02%' THEN '2012-02'
-            WHEN starttime LIKE '2012-03%' THEN '2012-03'
-            WHEN starttime LIKE '2012-04%' THEN '2012-04'
-            WHEN starttime LIKE '2012-05%' THEN '2012-05'
-            WHEN starttime LIKE '2012-06%' THEN '2012-06'
-            WHEN starttime LIKE '2012-07%' THEN '2012-07'
-            WHEN starttime LIKE '2012-08%' THEN '2012-08'
-            WHEN starttime LIKE '2012-09%' THEN '2012-09'
-            WHEN starttime LIKE '2012-10%' THEN '2012-10'
-            WHEN starttime LIKE '2012-11%' THEN '2012-11'
-            WHEN starttime LIKE '2012-12%' THEN '2012-12'
-            ELSE 'Total' END AS month,
-       SUM(slots) AS total_slots
-FROM bookings
-WHERE starttime >= '2012-01-01' AND starttime < '2013-01-01'
-GROUP BY facid, month
-WITH ROLLUP
-ORDER BY facid, month;
+USE cd; 
+SELECT 	IFNULL(derived_table.facid, 'Total') AS facid, IFNULL(derived_table.Month, 'Total') AS Month, SUM(derived_table.slots) AS 'Количество броней' 
+FROM (SELECT fac.facid AS facid, MONTH(book.starttime) AS month, SUM(book.slots) AS slots
+FROM facilities AS fac
+INNER JOIN bookings book ON fac.facid = book.facid
+WHERE YEAR(book.starttime) = 2012 
+GROUP BY facid, Month, slots) AS derived_table
+GROUP BY facid, Month WITH ROLLUP;
