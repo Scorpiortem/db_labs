28391ad1c751e855d838b722cfd0dec0f2e112b0
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-20 12:50:21 +0400

    task5.9

--- Lr5/task5.9.sql
+++ Lr5/task5.9.sql
@@ -2,12 +2,12 @@
 состоящую из названия объекта и дохода, отсортированного по доходу. Помните, что 
 для гостей и участников действуют разные цены!*/
 USE cd;
-SELECT f.facid AS Facility_name,
-       SUM(CASE WHEN mem.memid = 0 THEN f.guestcost * book.slots
-                ELSE f.membercost * book.slots END) AS Total_income
+SELECT fac.facility AS 'Название объекта',
+       SUM(CASE WHEN mem.memid = 0 THEN fac.guestcost * book.slots
+                ELSE fac.membercost * book.slots END) AS Total_income
 FROM bookings AS book
-INNER JOIN facilities AS f ON book.facid = f.facid
+INNER JOIN facilities AS fac ON book.facid = fac.facid
 LEFT JOIN members AS mem ON book.memid = mem.memid
-GROUP BY f.facid
+GROUP BY fac.facid
 HAVING Total_income < 1000
 ORDER BY Total_income DESC;
