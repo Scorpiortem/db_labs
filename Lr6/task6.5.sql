450ae72c4697a341dde8f4f46d248a01135aaa46
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-25 13:31:14 +0400

    task5

--- Lr6/task6.5.sql
+++ Lr6/task6.5.sql
@@ -2,4 +2,4 @@
 если они содержат в названии слово ‘Tennis’*/
 USE cd;
 SELECT UPPER(facility) AS 'Названия заглавными буквами' FROM facilities 
-WHERE facility LIKE '%Tennis%';
+WHERE facility LIKE '%TENNIS%';
