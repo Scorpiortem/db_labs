a3aa068de4f9c1759c214a4afca7949489a67293
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-25 12:58:05 +0400

    task18

--- Lr5/task5.18.sql
+++ Lr5/task5.18.sql
@@ -1,23 +1,26 @@
-/*Для каждого дня августа 2012 года рассчитайте скользящее 
-среднее общего дохода за предыдущие 15 дней. Вывод должен 
-содержать столбцы даты и дохода, отсортированные по дате. 
-Не забудьте учесть возможность того, что в день будет нулевой доход. 
-Примечание: используйте DATE_ADD для генерации серии дат.*/
 USE cd;
 WITH RECURSIVE DateRange AS (
-    SELECT '2012-08-01 00:00:00' AS Дата
+    SELECT '2012-08-01' AS Date
     UNION ALL
-    SELECT DATE_ADD(Дата, INTERVAL 1 DAY) 
+    SELECT DATE_ADD(Date, INTERVAL 1 DAY) 
     FROM DateRange 
-    WHERE Дата < '2012-08-31 23:59:59'
+    WHERE Date < '2012-08-31'
 ),
-RevenuePerDay AS (
-    SELECT DateRange.Дата, COALESCE(SUM(b.slots), 0) AS total_revenue
-    FROM DateRange
-    LEFT JOIN bookings b ON DATE(b.starttime) = DateRange.Дата
-    GROUP BY Дата
+TotalRevenue AS(
+SELECT Daterange.Date, 
+	Coalesce(SUM(
+        CASE 
+            WHEN b.memid = 0 THEN b.slots * f.guestcost 
+            ELSE b.slots * f.membercost
+        END), 0) as total_revenue
+    FROM Daterange
+    LEFT JOIN bookings b ON DATE(b.starttime) = Date
+    LEFT JOIN facilities f ON b.facid = f.facid
+    LEFT JOIN members m ON b.memid = m.memid
+    GROUP BY Date
 )
-SELECT Дата, ROUND((SELECT AVG(total_revenue) FROM RevenuePerDay RP 
-WHERE RP.Дата BETWEEN DATE_SUB(RPD.Дата, INTERVAL 14 DAY) AND RPD.Дата), 2) AS Скользящее_среднее
-FROM RevenuePerDay RPD
-ORDER BY Дата;
+ SELECT DISTINCT DATE, ROUND((SELECT SUM(total_revenue) FROM TotalRevenue TR  
+ WHERE b.starttime > TR.Date - INTERVAL 14 DAY AND b.starttime < TRD.Date + INTERVAL 1 DAY) / 15, 2) AS moving_average_revenue
+FROM TotalRevenue TRD
+LEFT JOIN bookings b ON DATE(b.starttime) = Date
+ORDER BY Date;
