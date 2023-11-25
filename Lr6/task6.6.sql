32578de278942caa8c3de88db171ae7dcb12552e
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-25 13:34:33 +0400

    task6

--- Lr6/task6.6.sql
+++ Lr6/task6.6.sql
@@ -2,4 +2,4 @@
 в названии слово  ‘Tennis’ в произвольном регистре.*/
 USE cd;
 SELECT facility AS 'Объекты' FROM facilities
-WHERE facility LIKE '%Tennis%' OR '%tennis%';
+WHERE facility LIKE '%Tennis%';
