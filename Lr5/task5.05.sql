3c4cf4e76b7ea3979c5f2f80f8820cfc76b69a9d
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-25 11:50:40 +0400

    task5

--- Lr5/task5.05.sql
+++ Lr5/task5.05.sql
@@ -3,5 +3,5 @@ USE cd;
 SELECT facility AS 'Объекты клуба', SUM(book.slots) AS 'Кол-во аренд'
 FROM facilities AS fac
 INNER JOIN bookings AS book ON book.facid = fac.facid
-WHERE book.starttime >= '2012-09-01 00:00:00' AND book.starttime <= '2012-09-30 23:59:59'
+WHERE book.starttime >= '2012-09-01' AND book.starttime < '2012-10-01'
 GROUP BY fac.facid;
