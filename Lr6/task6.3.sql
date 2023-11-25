b52b0d2150961c97238f521c6cc66053cc5b24ea
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-11-25 13:03:15 +0400

    task3

--- Lr6/task6.3.sql
+++ Lr6/task6.3.sql
@@ -1,10 +1,10 @@
 	/*Выберите процент использования объектов по месяцам, 
 	упорядочив по возрастанию*/
-SELECT facility AS 'Объекты',
+SELECT facility AS 'Объекты', YEAR(book.starttime) AS Год,
 MONTH(book.starttime) AS Месяц,
 (SUM(book.slots) / (SELECT SUM(slots) FROM bookings
 WHERE bookings.facid = fac.facility)) * 100 AS Процент_использования_объектов_в_месяц
 FROM bookings AS book
 INNER JOIN facilities AS fac ON book.facid = fac.facility
-GROUP BY fac.facility, Месяц
+GROUP BY fac.facility, Месяц, Год
 ORDER BY Процент_использования_объектов_в_месяц ASC;
