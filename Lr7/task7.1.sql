ebd4ec5a08cbdcbbba1fbe79ec2d966213823f99
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-12-03 20:41:23 +0400

    new try

--- Lr7/task7.1.sql
+++ Lr7/task7.1.sql
@@ -4,17 +4,14 @@ USE cd;
 DELIMITER $$
 DROP FUNCTION IF EXISTS CalculateRentalCost;
 CREATE FUNCTION CalculateRentalCost(memid INT, facid INT, slots INT)
-RETURNS DECIMAL(10, 2)
+RETURNS INT
 DETERMINISTIC
 BEGIN
-    DECLARE Стоимость_аренды DECIMAL(10, 2);
-    SELECT 
-        CASE 
-            WHEN memid = 0 THEN guestcost * slots
-            ELSE membercost * slots
-        END INTO Стоимость_аренды
-    FROM facilities
-    WHERE facid = facilities.facid;
+    DECLARE Стоимость_аренды INT;
+    SET Стоимость_аренды = (SELECT 
+			IF(memid = 0, guestcost, membercost) * slots
+			FROM facilities
+			WHERE facid = facilities.facid);
     RETURN Стоимость_аренды;
 END$$
 DELIMITER ;
