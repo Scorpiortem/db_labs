85e964b173e1af8173c718f6d6b1f3be15b70a62
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-12-04 20:58:47 +0400

    решение разделено на части

--- Lr7/task7.2(create_table).sql
+++ Lr7/task7.2(create_table).sql
@@ -1,6 +1,6 @@
 USE cd;
 CREATE TABLE payments (
-  payid INT PRIMARY KEY,
+  payid INT PRIMARY KEY AUTO_INCREMENT,
   bookid INT,
   payment DECIMAL,
   FOREIGN KEY (bookid) REFERENCES bookings(bookid)
