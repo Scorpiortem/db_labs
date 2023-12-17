6ebdaf3ee18412dd509344d2a881270c7a624927
Scorpiortem <147166471+Scorpiortem@users.noreply.github.com>
2023-12-17 16:25:08 +0400

    updated

--- Lr7/task7.3.sql
+++ Lr7/task7.3.sql
@@ -5,16 +5,18 @@ USE cd;
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS payback $$
-CREATE PROCEDURE payback()
+CREATE PROCEDURE payback(currentDate DATE)
+READS SQL DATA
+NOT DETERMINISTIC
   BEGIN
     SELECT book.facid, fac.facility, SUM(pay.payment) - fac.monthlymaintenance AS Доход
 	FROM bookings AS book
 	JOIN payments AS pay ON book.bookid = pay.bookid
 	JOIN facilities AS fac ON book.facid = fac.facid
-	WHERE DATE(starttime) < '2012-08-01' AND DATE(starttime) >= '2012-07-01'
+	WHERE DATE_FORMAT(starttime, '%y %m') = DATE_FORMAT(currentDate, '%y %m')
 	GROUP BY book.facid ORDER BY book.facid;
   END $$
 
 DELIMITER ;
 
-CALL payback;
+CALL payback('2012-07-18');
